4b794ce1fb67ff6afee590e151c967779f2ccb68
clintaveben <44338892+clintaveben@users.noreply.github.com>
2018-10-23 13:41:19 +0530

    Add files via upload

---
+++ Assignment1.1.r
@@ -0,0 +1,38 @@
+# 1.	How many ways are there to call a function in R ?
+
+Ans :  Functions are used to logically break our code into simpler parts 
+which become easy to maintain and understand. There are 3 methods to call 
+a function in R  as follows:
+  
+  a)	Arguments
+  b)	Body
+  c)	Environment
+The list of argument names are contained within parentheses. 
+Next, the body of the function-the statements that are executed 
+when it runs-is contained within curly braces ({}). The statements 
+in the body are indented by two spaces, which makes the code easier 
+to read but does not affect how the code operates.
+
+# 2.	Is the below statement true?
+
+.	The lazy evaluation of a function means, the argument is evaluated 
+only if it is evaluated only if it is used inside body of the function.
+
+Ans : True
+
+# 3.	Mention true or false for the below statement 
+
+a.	Insights driven from descriptive analytics is not meaningful. 
+Ans : False 
+
+b.	The number of values in each element of a list, should be equal.
+Ans : False
+
+c.	Dataset are not stored in memory of the computer using R.
+Ans: False
+
+d.	Data frames and metrices are two dimensional however the array
+is multidimensional. 
+
+Ans: True
+
---
+++ Assignment1.r
@@ -0,0 +1,26 @@
+# Problem Statements
+
+1.	Please mention true or false for the below statement
+
+a)	Prescriptive analytics is used to predicts the future outcomes 
+
+Ans : False
+Predictive analytics is used to predicts future outcomes
+
+b)	Base R packages installed automatically
+Ans : True
+
+2.	What is recycling  of elements in a Vector
+
+Ans : Recycling occurs when vector arithmetic  is performed on multiple 
+vectors of different size. R takes the shorter vector repeats it until 
+it becomes long enough to match the longer one.
+
+
+3.	Give an example of recycling of elements
+
+Ans : suppose one vector a= c (1,2,3,4,5,6) and other b= c(1,3). 
+When we add these two vectors  a+b, it will take the shorter vector 
+and repeat them until it becomes long enough to match the longer one 
+as c(1,3,1,3,1,3) and gives us the result as [1] 2,5,4,7,6,9
+
